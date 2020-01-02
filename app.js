f984c6b5cbb3af147ae9163cd3628fc74d9f569e
Suhel-PC\Suhel <suhelkhan275@gmail.com>
2020-01-02 06:20:39 +0530

    modified

--- app.js
+++ app.js
@@ -1,193 +1,67 @@
-/************* this is example how file system module works *************************************** 
+const yargs = require('yargs');
+const notes = require('./notes.js');
+yargs.version('1.1.0');
 
-const fs = require('fs')// if we require any [node.js] module or [npm] module etc in our currentfile
-// we have to load this module into current file , to load we use [require], here we are loading 
-// [file system module] that is use to read files , write files etc. here we created const variable
-// with [fs] name it may be any name but it is industry standard that it should same as module name
-// because it will be easy to work in cross project
+yargs.command({
+  command: 'add',
+  describe: 'add the note',
+  builder: {
+    title: {
+      describe: 'note title',
+      demandOption: true,
+      type: 'string'
+    },
 
-fs.writeFileSync('notes.txt','This is first time!!');// [fs] is an object and [writeFileSync] is a 
-// function of [fs] object , this is sysncronous function , asyncronous function we will see later
-// first argument is a file name and second argument is conten of the file, now we are using [text]
-// document later we will use other type, so we will run then this function is file does not exist
-// then it will create and put the content , if file already exist then it will overwrite the 
-// content
-fs.writeFileSync('notes.txt','This is second time!!');// here it will overwrite the content
-fs.appendFileSync('notes.txt',' here we are checking append function!!!!!');// [appendFileSync] function
-// will not overwrite the content it will add the content [this was the coding challenge]
-
-*****************************************************************************************************/
-
-//const firstName = require('./utils.js');// here we are loading name variable present in [utils.js] but
-// it should be in same directory , if any [console] statemennt present in [utils.js] file 
-// then also it will print when we will run [app.js] 
-//console.log(firstName);// it will print the variable that is present in [util.js] file
-
-/******************** this is how we can import our created file  ************************************
-const sum = require('./utils.js');// it will load [add] function 
-const msg = require('./notes.js');// it will load getNotes function [it was challenge]
-console.log(sum(4,-2));
-console.log(msg());
-
-console.log('app.js');
-
-*************************************************************************************************/
-
-/*********************** This is how to use validator ********************************************
- 
-const validator = require('validator');// here we are loading [validator] package into our file,
-// if you goto to documentation of [validator] there you will see [no ES6 or ES6] , in ES6 to 
-// import another file we have to use [import] but in [node.js] we use [require]
-// [validator] is used to validate the data we can do manually also but it is better to use library when
-// we have, there are many methods in [validator] that you can use in [validator] documentation
-
-console.log(validator.isEmail('suhelkhan275@gmail.com'));// it will return true because it is valid email
-
-console.log(validator.isEmail('suhelkhan275gmail.com'));// it will return false because it is invalid email
-
-console.log(validator.isURL('https://google.com'));//  it will return true because it is valid URL
-
-console.log(validator.isURL('https//google.com'));// it will return false because it is invalid URL
-
-******************************************************************************************************/
-
-/************************* This is how to use chalk [it was the coding challenge] ********************
- 
-const chalk = require('chalk');// here we are loading [chalk] module that is used to style our consoleoutput
-console.log(chalk.bold.green.inverse('success!'));// here we are using the properties of [chalk], and does
-// not depend that in which sequence we are using these properties
-
-******************************************************************************************************/
-
-/*************** how we get argument from cmp by process **********************************************
- 
-console.log(process.argv);// full form of [argv] is [argument vector] , as we discussed in 2nd section 
-// that [process] run globlly, output of this will be an array [index 1] = location where node is installed
-// [index 2] = location where current file is saved , remaaning index will be argumnets as it is you passed
-// in teminal
-console.log(process.argv[2]);// it will print extra arguments that you passed 
-
-const command = process.argv[2];
-
-if(command === 'add'){// If you pass [add] extra on terminal , remember it reads as string from terminal
-    console.log('add the note !');
-}else if(command === 'remove'){// If you pass [remove] extra on terminal , remember it reads as string
-    // from terminal
-    console.log('remove the note !!');
-}
-
-*****************************************************************************************************/
-
-const yargs = require('yargs');// here we are loading yargs package check the notes to know more
-const notes = require('./notes.js');//  here we are importing the object that [notes.js] file exported
-// customize yargs version
-yargs.version('1.1.0');// here we are setting yargs version , if we not set then it will give default
-// version
-
-// creating add command
-yargs.command({// [command] is a function of yargs package where we pass object and as a property of 
-    // this object we can perform our task
-   command:'add',// this is what user pass as argument then our defined task will perform
-   describe:'add the note',// this is description of that task, it is not mendatory to give 
-   // desription but it is good practice
-
-   builder:{// builder will arrange the command that will be pass after parent command in this case after
-    // [add] command
-       title:{// here after add command we can give title like [--title="Examination"]
-           describe:'note title',// this is description like [command description] that we gave above
-           demandOption: true ,// here we are applying the condition that title should be there after [add]
-           type: 'string'// here we are telling that title type will be string otherwise if we don't 
-           // assign title then it will take boolean value that we gave in [demandOption]
-       },
-
-       body:{// we can define body before [title] or after [title] no problem
-           describe: 'note body',
-           demandOption: true,
-           type: 'string'
-       }
-   },
-   handler(argv){// here we can define function and perform task
-      //console.log('adding note!!'); // this is to check handler is working or not
-      //console.log(argv);// it will give array that contains more information also
-      //console.log('title: '+ argv.title);// it will print our title
-      //console.log('body: ' + argv.body);// it will print our body
-      notes.addNote(argv.title,argv.body);// here we will call [addNote] function that is created in 
-      // [note.js] file and this function will add our title and body  that we will give in command to 
-      // [note.json] file
-   }
-})
+    body: {
+      describe: 'note body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
 
 // creating remove command
 yargs.command({
-    command:'remove',
-    describe:'remove note',
-    builder:{
-        title:{
-            describe:'this is the title that will be removed',
-            demandOption:true,
-            type:'string'
-        }
-
-    },
-    handler(argv){
-        //console.log('removing note');
-        notes.removeNote(argv.title);
+  command: 'remove',
+  describe: 'remove note',
+  builder: {
+    title: {
+      describe: 'this is the title that will be removed',
+      demandOption: true,
+      type: 'string'
     }
-})
+  },
+  handler(argv) {
+    notes.removeNote(argv.title);
+  }
+});
 
 // creating read command
 yargs.command({
-  command:'read',
-  describe:'reading note',
-  builder:{
-     title:{
-         describe:'give the title that you want to read',
-         demandOption:true,
-         type:'string'
-     }
+  command: 'read',
+  describe: 'reading note',
+  builder: {
+    title: {
+      describe: 'give the title that you want to read',
+      demandOption: true,
+      type: 'string'
+    }
   },
-  handler(argv){
-      //console.log('reading note');
-      notes.readNote(argv.title);
+  handler(argv) {
+    notes.readNote(argv.title);
   }
-
-})
+});
 
 // creating list command
 yargs.command({
-    command:'list',
-    describe:'list note',
-    handler(){
-        //console.log('listing out some notes');
-        notes.listNotes();
-    }
-})
-//console.log(process.argv);// as we declared that it will give array
-//console.log(yargs.argv);// here we will get argument information in a sequence way
+  command: 'list',
+  describe: 'list note',
+  handler() {
+    notes.listNotes();
+  }
+});
 
-yargs.parse();// when compiler read this [parse] line then compiler will go through all parsing code that 
-// we did , we can also use [yargs.argv] but it will print one array will all command info that we don't
-// want so we are using [yargs.parse()]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+yargs.parse();
--- notes.js
+++ notes.js
@@ -1,107 +1,69 @@
-const fs = require('fs');// here we are impoeting [file system] that we know
+const fs = require('fs');
 const chalk = require('chalk');
+const addNote = (title, body) => {
+  const notes = loadNotes();
 
-//const getNotes = ()=>'Your notes ..........'// this is the example function to check how function is exported 
+  const duplicateNote = notes.find(note => note.title === title);
 
+  if (!duplicateNote) {
+    notes.push({
+      title: title,
+      body: body
+    });
 
-const addNote = (title,body)=> {//this function will add  title and body  that are passed from [app.js] 
-    // file and will add to [note.json] file
-    const notes = loadNotes();// this function will fetch the data from [notes.json] file and convert it
-    // into array and here we will store in [notes] variable
-    //console.log(notes);// it is only for checking purpose
+    saveNotes(notes);
+    console.log(chalk.green.inverse('New note is added :)'));
+  } else {
+    console.log(chalk.red.inverse('Same title is present :('));
+  }
+};
 
-    //const duplicateNotes = notes.filter((note)=>note.title === title)// here we are checking that same [title] should 
-    // not be present previously. [filter] function will return the [sub array] of [notes] array.
-    // in filter function we can write a function for matching criteria , this matching function will
-    // run for every element of notes array. if filter function gets return [true] then it will put
-    // this current element into sub array, if get false then filter function will not put this current 
-    // element to the sub array.if title match then it will return true
-    // but problem is this [filter] method after finding duplicate element will not stop it will search for
-    // remaining elements also , so we will use [find] method because [find] method after finding our 
-    // required element will stop
+const loadNotes = () => {
+  try {
+    const bufferData = fs.readFileSync('notes.json');
+    const dataJSON = bufferData.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
 
-    const duplicateNote = notes.find((note)=>note.title === title);
+const saveNotes = notes => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
 
-    //if(duplicateNotes.length === 0){// this is the statement if we use [filter]
-    if(!duplicateNote){// we can also use if(duplicateNote === undefined) , both are correct because if 
-        // [find] method does'nt find element then it returns [undefined]
-        notes.push({// here we are pusing the new object into array where all previous object are present
-            // that we fetch by [loadNotes] function, we did this because if without fetching previos object 
-            // if we put new then it will overwrite and by doing this all object will be present in one array
-            title:title,
-            body:body
-        })
+const removeNote = title => {
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(note => note.title !== title);
+  if (notesToKeep.length < notes.length) {
+    saveNotes(notesToKeep);
+    console.log(chalk.green.inverse('Note Removed !'));
+  } else {
+    console.log(chalk.red.inverse('No Note Found !'));
+  }
+};
+const listNotes = () => {
+  console.log(chalk.blue.inverse('Your Notes:'));
+  const notes = loadNotes();
+  notes.forEach(note => console.log(chalk.yellow.inverse(note.title)));
+};
 
-        saveNotes(notes);// this function will store our final array into [note.json] file
-        console.log(chalk.green.inverse('New note is added :)'));
-    }else{
-        console.log(chalk.red.inverse('Same title is present :('));
-    }
-
-  
-
-
-}
-
-const loadNotes =()=>{// this function will fetch the data from [notes.json] file and convert it
-    // into array and here we will store in [notes] variable
-    try{// here we apply try because in first time [notee.json] file will not be there so these codes will
-        // create error
-        const bufferData = fs.readFileSync('notes.json');
-        const dataJSON = bufferData.toString();
-        return JSON.parse(dataJSON);
-    }catch(e){
-        return [];// if error occure that means [notes.json] file is not there so we will return an empty
-        // array and new object will be pushed in this empty array and saved into [notes.json] file , so 
-        // here we have decided that all object will be present in array
-    }
-   
-}
-
-const saveNotes =(notes)=>{// this function will store our final array into [note.json] file
-const dataJSON = JSON.stringify(notes);
-fs.writeFileSync('notes.json', dataJSON);
-
-}
-
-const removeNote =(title)=>{
-    // console.log(`this title ${title} will be removed`);
-    const notes = loadNotes();
-        const notesToKeep = notes.filter((note)=>note.title !== title )
-        if(notesToKeep.length < notes.length){
-            saveNotes(notesToKeep);
-            console.log(chalk.green.inverse('Note Removed !'));
-
-        }else{
-            console.log(chalk.red.inverse('No Note Found !'));
-        }
-}
-const listNotes = ()=>{
-    console.log(chalk.blue.inverse('Your Notes:'))
-    const notes =loadNotes();
-    notes.forEach((note)=> console.log(chalk.yellow.inverse(note.title))); 
-}
- 
-const readNote = (title)=>{
-    const notes = loadNotes();
-    const note = notes.find((note)=>note.title === title);
-    if(note){
-    console.log(chalk.green.inverse(note.title)+':'+chalk.yellow.inverse(note.body));
-    }else{
+const readNote = title => {
+  const notes = loadNotes();
+  const note = notes.find(note => note.title === title);
+  if (note) {
+    console.log(
+      chalk.green.inverse(note.title) + ':' + chalk.yellow.inverse(note.body)
+    );
+  } else {
     console.log(chalk.red.inverse('Sorry this title is not present :('));
-    }
-}
+  }
+};
 
-
-// module.exports = getNotes ;// members of a file have scope in the file but if we want to use these 
-// members in another file then we have to export . [It was coding challenge].
-
-module.exports = {// if we have to require multiple export then we will export an object and put all things
-    // as this object property
-    //getNotes:getNotes,// here we are saying that we are exporting [getNotes] function that can be used 
-    //after importing by [getNotes] name, it was for checking purpose.0-
-    addNote:addNote,
-    removeNote:removeNote,
-    listNotes:listNotes,
-    readNote:readNote
-}
+module.exports = {
+  addNote: addNote,
+  removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote
+};
